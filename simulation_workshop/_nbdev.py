f85999a38e2eb0986c109e131016c201899168ca
hgzech <hilmarzech@gmail.com>
2021-11-17 17:30:27 +0200

    Added simulation notebook

--- simulation_workshop/_nbdev.py
+++ simulation_workshop/_nbdev.py
@@ -4,10 +4,10 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {}
 
-modules = []
+modules = ["simulation.py"]
 
 doc_url = "https://hgzech.github.io/simulation_workshop/"
 
-git_url = "https://github.com/hgzech/simulation_workshop/tree/main/"
+git_url = "https://github.com/hgzech/simulation_workshop/tree/master/"
 
 def custom_doc_links(name): return None
